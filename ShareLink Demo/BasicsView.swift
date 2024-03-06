899e2c657b38f8782dddbd08c360a30c8e1ce204
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2024-03-06 16:31:48 +0300

    Complete

--- ShareLink Demo/BasicsView.swift
+++ ShareLink Demo/BasicsView.swift
@@ -13,12 +13,36 @@ struct BasicsView: View {
     var body: some View {
         NavigationStack {
             VStack(alignment: .leading, spacing: 10) {
-                // Basic Links
+                ShareLink(item: "String Item")
+                ShareLink(item: URL(string: "https://www.apple.com")!)
+                ShareLink("Some Text", item: URL(string: "https://www.apple.com")!)
+                ShareLink(Text("\(Image(systemName: "star.fill")) - with SFImage"), item: URL(string: "https://www.apple.com")!)
+                ShareLink(item: "Item Subject - No Preview", subject: Text("Subject")) {
+                    Text("Title")
+                }
+                ShareLink(item: "Item Subject Message", subject: Text("Subject"), message: Text("The message")) {
+                    Text("Title")
+                }
+                ShareLink(item: URL(string: "https://www.apple.com")!, subject: Text("Subject"), message: Text("The message")) {
+                    Text("Title")
+                }
+                ShareLink(item: URL(string: "https://www.apple.com")!, subject: Text("Subject"), message: Text("The message")) {
+                    HStack {
+                        Text("Title")
+                        Spacer()
+                        Image(systemName: "star.fill")
+                    }
+                }
                 Spacer()
             }
             .font(.title)
             .padding()
             .navigationTitle("ShareLinks Basics")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    ShareLink(item: "Item to Share")
+                }
+            }
         }
     }
 }
--- ShareLink Demo/WineView.swift
+++ ShareLink Demo/WineView.swift
@@ -10,22 +10,52 @@ import SwiftUI
 
 struct WineView: View {
     let wine = Wine.sample
+    @State private var renderedImage: Image?
+    
     var body: some View {
         NavigationStack{
-            VStack(alignment: .leading) {
-                Text(wine.variety).font(.largeTitle).bold()
-                Text(wine.name).font(.title)
-                Text("\(wine.flag) \(wine.region)").italic()
-                Text(wine.localPrice)
-                Text("⭐️ \(wine.rating)")
-                wine.labelImage
-                    .resizable()
-                    .scaledToFit()
-                    .clipShape(RoundedRectangle(cornerRadius: 20))
-                    .frame(width: 250)
+            VStack {
+                WineLabel(wine: wine)
+                    .onAppear {
+                        let renderer = ImageRenderer(content: WineLabel(wine: wine))
+                        renderer.scale = 3
+                        if let image = renderer.cgImage {
+                            renderedImage = Image(decorative: image, scale: 1.0)
+                        }
+                    }
+                
+                if let renderedImage = renderedImage {
+                    ShareLink("Share Label as Image",
+                              item: renderedImage,
+                              subject: Text(wine.winery),
+                              message: Text("Check out this wine label!"),
+                              preview: SharePreview(wine.label, image: renderedImage))
+                              
+                }
+                
                 Spacer()
             }
             .navigationTitle(wine.winery)
+        }
+    }
+}
+
+struct WineLabel: View {
+    
+    let wine: Wine
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(wine.variety).font(.largeTitle).bold()
+            Text(wine.name).font(.title)
+            Text("\(wine.flag) \(wine.region)").italic()
+            Text(wine.localPrice)
+            Text("⭐️ \(wine.rating)")
+            wine.labelImage
+                .resizable()
+                .scaledToFit()
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .frame(width: 250)
         }
     }
 }
